57e288483e1562e9e6c1880cb0f47df6930604a5
mbtts <matthewbetts@me.com>
2018-06-17 06:37:11 +0100

    Add id to contacts

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 const api = async () => {
-  const result = await fetch("http://www.mocky.io/v2/581335f71000004204abaf83");
+  const result = await fetch(
+    "https://www.mocky.io/v2/581335f71000004204abaf83"
+  );
   const json = await result.json();
-  return json.contacts;
+  return json.contacts.map((contact, i) => ({ id: i, ...contact }));
 };
 
 export default api;
